6a243cc4c1a13dc205d1b337514ac1691f1c0c72
ketanmakwana30 <ketanmakwana0130@gmail.com>
2025-12-14 01:14:28 +0530

    add opetators

---
+++ Lecture-1/Part-1/script.js
@@ -0,0 +1,54 @@
+let a = 12;
+let b = 20;
+let c = 39;
+
+// console.log("sum is a and b",a + c);
+
+// let sum = `The total sum is a, b and c ${a + b + c}.`;
+console.log(`The total sum is a, b and c ${a + b + c}.`);
+
+
+// Arithmetic Operators
+let x = 10;
+let y = 20;
+
+console.log(x + y);
+console.log(x - y);
+console.log(x * y);
+console.log(x / y);
+console.log(x % y);
+console.log(x ** y);
+
+
+//Unary Opetators
+x = 10;
+
+console.log(x++); //10
+console.log(++x); //12
+
+
+//Assignment Opetators
+x = 10;
+y = 20;
+
+console.log(x = y);
+console.log(x += y);
+console.log(x -= y);
+console.log(x *= y);
+console.log(x /= y);
+console.log(x %= y);
+
+
+//Comparison Opetators
+let age = 18;
+
+console.log(age = 18);
+console.log(age > 18);
+console.log(age >= 18);
+console.log(age < 18);
+console.log(age <= 18);
+console.log(age == 18);
+console.log(age != 18);
+
+
+//Logical Opetators
